02e72aa9e2687bb22f1642fd1d21a84473c56be8
AlineDB <aline.debarros@student.hepl.be>
2022-02-09 16:56:33 +0100

    Initial commit

---
+++ wp-content/themes/dw/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html  lang="fr" >
+<head>
+    <meta charset="utf8-mb4" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+</head>
+<h1>
+    Hello World
+</h1>
+<body>
+
+</body>
+</html>
